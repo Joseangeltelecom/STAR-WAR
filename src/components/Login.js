9ee51b4a608598e85d8210d9dbff404d2fcb369e
Jose Angel <joseangeltelecom@gmail.com>
2022-03-09 11:53:13 -0500

    adding basename to the router

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-// import logo from "../images/logo.png"
+import logo from "../images/logo.png"
 import { useContext } from "react"
 import { UserContext } from "../context/userContext"
 const CryptoJS = require("crypto-js")
